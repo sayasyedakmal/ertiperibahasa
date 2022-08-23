1ed96e52de001f5422c2807bfc5aaec5056dc7d8
Syed_Akmal <syedakmalofficial@gmail.com>
2022-08-23 18:39:44 +0800

    adding and using ejs

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const portNumber = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
-});
-
-app.get('/', (req, res) => {
-    res.send('LAMAN WEB ERTIPERIBAHASA YANG BARU! <br> OOOOH YEAH');
 });
